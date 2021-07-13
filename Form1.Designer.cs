1d68fbd3f7b99ed62922ae8aa2fc6812d7f4087c
markj30m <59736485+markj30m@users.noreply.github.com>
2021-07-13 13:46:43 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Rock_Paper_and_Scissors_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbxPlayer = new System.Windows.Forms.PictureBox();
+            this.pbxAI = new System.Windows.Forms.PictureBox();
+            this.countDowntimer = new System.Windows.Forms.Timer(this.components);
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissor = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.lblPlayername = new System.Windows.Forms.Label();
+            this.lblAI = new System.Windows.Forms.Label();
+            this.lblscore = new System.Windows.Forms.Label();
+            this.lblCountdown = new System.Windows.Forms.Label();
+            this.lblRounds = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxAI)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxPlayer
+            // 
+            this.pbxPlayer.Image = global::Rock_Paper_and_Scissors_Game.Properties.Resources.question;
+            this.pbxPlayer.Location = new System.Drawing.Point(171, 63);
+            this.pbxPlayer.Name = "pbxPlayer";
+            this.pbxPlayer.Size = new System.Drawing.Size(130, 130);
+            this.pbxPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxPlayer.TabIndex = 0;
+            this.pbxPlayer.TabStop = false;
+            // 
+            // pbxAI
+            // 
+            this.pbxAI.Image = global::Rock_Paper_and_Scissors_Game.Properties.Resources.question;
+            this.pbxAI.Location = new System.Drawing.Point(592, 63);
+            this.pbxAI.Name = "pbxAI";
+            this.pbxAI.Size = new System.Drawing.Size(130, 130);
+            this.pbxAI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxAI.TabIndex = 1;
+            this.pbxAI.TabStop = false;
+            // 
+            // countDowntimer
+            // 
+            this.countDowntimer.Interval = 1000;
+            this.countDowntimer.Tick += new System.EventHandler(this.countDowntimerEvent);
+            // 
+            // btnRock
+            // 
+            this.btnRock.Location = new System.Drawing.Point(41, 77);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(75, 23);
+            this.btnRock.TabIndex = 2;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Location = new System.Drawing.Point(41, 123);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(75, 23);
+            this.btnPaper.TabIndex = 3;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnScissor
+            // 
+            this.btnScissor.Location = new System.Drawing.Point(41, 166);
+            this.btnScissor.Name = "btnScissor";
+            this.btnScissor.Size = new System.Drawing.Size(75, 23);
+            this.btnScissor.TabIndex = 4;
+            this.btnScissor.Text = "Scissor";
+            this.btnScissor.UseVisualStyleBackColor = true;
+            this.btnScissor.Click += new System.EventHandler(this.btnScissor_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(746, 260);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(75, 23);
+            this.btnHelp.TabIndex = 5;
+            this.btnHelp.Text = "Restart";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // lblPlayername
+            // 
+            this.lblPlayername.AutoSize = true;
+            this.lblPlayername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayername.Location = new System.Drawing.Point(178, 32);
+            this.lblPlayername.Name = "lblPlayername";
+            this.lblPlayername.Size = new System.Drawing.Size(123, 24);
+            this.lblPlayername.TabIndex = 6;
+            this.lblPlayername.Text = "PlayerName";
+            this.lblPlayername.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblAI
+            // 
+            this.lblAI.AutoSize = true;
+            this.lblAI.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAI.Location = new System.Drawing.Point(644, 32);
+            this.lblAI.Name = "lblAI";
+            this.lblAI.Size = new System.Drawing.Size(29, 24);
+            this.lblAI.TabIndex = 7;
+            this.lblAI.Text = "AI";
+            this.lblAI.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblscore
+            // 
+            this.lblscore.AutoSize = true;
+            this.lblscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblscore.Location = new System.Drawing.Point(405, 76);
+            this.lblscore.Name = "lblscore";
+            this.lblscore.Size = new System.Drawing.Size(97, 24);
+            this.lblscore.TabIndex = 8;
+            this.lblscore.Text = "Win/Lose";
+            // 
+            // lblCountdown
+            // 
+            this.lblCountdown.AutoSize = true;
+            this.lblCountdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountdown.Location = new System.Drawing.Point(432, 141);
+            this.lblCountdown.Name = "lblCountdown";
+            this.lblCountdown.Size = new System.Drawing.Size(21, 24);
+            this.lblCountdown.TabIndex = 9;
+            this.lblCountdown.Text = "5";
+            // 
+            // lblRounds
+            // 
+            this.lblRounds.AutoSize = true;
+            this.lblRounds.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRounds.Location = new System.Drawing.Point(405, 201);
+            this.lblRounds.Name = "lblRounds";
+            this.lblRounds.Size = new System.Drawing.Size(82, 24);
+            this.lblRounds.TabIndex = 10;
+            this.lblRounds.Text = "Rounds";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 311);
+            this.Controls.Add(this.lblRounds);
+            this.Controls.Add(this.lblCountdown);
+            this.Controls.Add(this.lblscore);
+            this.Controls.Add(this.lblAI);
+            this.Controls.Add(this.lblPlayername);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnScissor);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Controls.Add(this.pbxAI);
+            this.Controls.Add(this.pbxPlayer);
+            this.Name = "Form1";
+            this.Text = "Rock Paper Scissors";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxAI)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxPlayer;
+        private System.Windows.Forms.PictureBox pbxAI;
+        private System.Windows.Forms.Timer countDowntimer;
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissor;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Label lblPlayername;
+        private System.Windows.Forms.Label lblAI;
+        private System.Windows.Forms.Label lblscore;
+        private System.Windows.Forms.Label lblCountdown;
+        private System.Windows.Forms.Label lblRounds;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rock_Paper_and_Scissors_Game
+{
+    public partial class Form1 : Form
+    {
+
+        int rounds = 3;
+        int timerPerRound = 6;
+        bool gameOver = false;
+        // uses Random for the AI choices added multiple answers in array to give me variety to the rng
+        string[] AIChoiceList = {"rock","paper","scissor","paper","scissor","rock" };   
+        int randomNumber = 0;
+        Random rng = new Random();
+
+        string AIChoice;
+        string playerChoice;
+
+        int AIScore;
+        int playerScore;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            countDowntimer.Enabled = true;
+
+            playerChoice = "none";
+
+            lblCountdown.Text = "5";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            pbxPlayer.Image = Properties.Resources.rock;
+            playerChoice = "rock";
+
+
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            pbxPlayer.Image = Properties.Resources.paper;
+            playerChoice = "paper";
+
+        }
+
+        private void btnScissor_Click(object sender, EventArgs e)
+        {
+            pbxPlayer.Image = Properties.Resources.scissors;
+            playerChoice = "scissor";
+
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            playerScore = 0;
+            AIScore = 0;
+            rounds = 3;
+            lblscore.Text = "Player: " + playerScore + " - " + "AI " + AIScore;
+
+            playerChoice = "none";
+
+            countDowntimer.Enabled = true;
+
+            pbxPlayer.Image = Properties.Resources.question;
+            pbxAI.Image = Properties.Resources.question;
+
+            gameOver = false;
+
+
+        }
+
+        private void countDowntimerEvent(object sender, EventArgs e)
+        {
+            timerPerRound -= 1;
+            lblCountdown.Text = timerPerRound.ToString();
+
+            lblRounds.Text = "Rounds:" + rounds; 
+
+            if(timerPerRound < 1)
+            {
+                countDowntimer.Enabled = false;
+
+                timerPerRound = 6;
+
+                randomNumber = rng.Next(0, AIChoiceList.Length);
+
+                AIChoice = AIChoiceList[randomNumber];
+
+                switch(AIChoice)
+                {
+                    case "rock":
+                        pbxAI.Image = Properties.Resources.rock;
+
+                        break;
+                    case "paper":
+                        pbxAI.Image = Properties.Resources.paper;
+
+                        break;
+                    case "scissor":
+                        pbxAI.Image = Properties.Resources.scissors;
+
+                        break;
+
+                }
+
+                if (rounds > 1)
+                {
+                    checkGame();
+                }
+                else
+                {
+                    if (playerScore > AIScore)
+                    {
+                        MessageBox.Show("playerScore Won the game");
+                    }
+                    else
+                    {
+                        MessageBox.Show("AI Won the game");
+                    }
+
+                    gameOver = true;
+                }
+                    
+
+            }
+
+ 
+
+        }
+        private void checkGame()
+        {
+            // if statement to find who wins and give them a point
+            if(playerChoice == "rock" && AIChoice == "paper")
+            {
+                AIScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("AI Wins Paper Covers Rock");
+            }
+          else if (playerChoice == "scissor" && AIChoice == "rock")
+            {
+                AIScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("AI Wins Rock Breaks Scissor");
+            }
+          else if (playerChoice == "paper" && AIChoice == "scissor")
+            {
+                AIScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("AI Wins scissor cuts paper");
+            }
+           else if (playerChoice == "rock" && AIChoice == "scissor")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins Rock Breaks Scissor");
+            }
+            else if (playerChoice == "paper" && AIChoice == "rock")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins Paper Covers Rock");
+            }
+            else if (playerChoice == "scissor" && AIChoice == "paper")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins scissor cuts paper");
+            }
+            else if (playerChoice == "none")
+            {
+                MessageBox.Show("Make A choice");
+            }
+            else
+            {
+                MessageBox.Show("Draw");
+            }
+            startNextRound();
+        }
+
+        private void startNextRound()
+        {
+
+            if(gameOver == true)
+            {
+                return;
+            }
+
+
+            lblscore.Text = "Player: " + playerScore + " - " + "AI " + AIScore;
+
+            playerChoice = "none";
+
+            countDowntimer.Enabled = true;
+
+            pbxPlayer.Image = Properties.Resources.question;
+            pbxAI.Image = Properties.Resources.question;
+        }
+
+    }
+}
